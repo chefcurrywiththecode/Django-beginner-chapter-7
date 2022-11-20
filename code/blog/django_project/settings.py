9abff7a3924c6eb8339ab027427db9a65aeccd3d
jemberen <nathaniel.jembere@cshs.org>
2022-11-19 20:05:21 -0800

    forms and user accounts

--- code/blog/django_project/settings.py
+++ code/blog/django_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-0ai2k=&xz4=fd=gfz=ogf-q@l^d=*jqeo^6l!e*m!&qa!a5by_
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [".herokuapp.com", "localhost", "127.0.0.1"]
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
